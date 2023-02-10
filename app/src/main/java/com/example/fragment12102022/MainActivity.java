5d41f6af0b2ac0136eaf12c71ba3288320e0c1d1
Phat <phatdroid94@gmail.com>
2023-02-10 18:17:54 +0700

    add fragment by code

--- app/src/main/java/com/example/fragment12102022/MainActivity.java
+++ app/src/main/java/com/example/fragment12102022/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.fragment12102022;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnAddFragment;
+    FragmentManager fragmentManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fragmentManager = getSupportFragmentManager();
+        btnAddFragment = findViewById(R.id.button_add_fragment);
+        btnAddFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                AndroidFragment androidFragment = new AndroidFragment();
+                fragmentTransaction.add(R.id.linear_container_fragment, androidFragment);
+                fragmentTransaction.commit();
+            }
+        });
     }
 }
